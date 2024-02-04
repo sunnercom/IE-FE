a6daa48c7b2740c3572f6c70ce481d121ff32efd
sunnercom <nuriddinovsunnatbek9@gmail.comm>
2024-02-04 22:36:23 +0500

    ...

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+"use strict"
+//Ro'yhatdan o'tish qismi
 document.addEventListener("DOMContentLoaded", function() {
     let signupForm = document.getElementById("signup-form");
   
@@ -16,3 +18,47 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   });
   
+
+
+// Timer chegirma
+var startTime = Date.now();
+
+var startHours = 10;
+var startMinutes = 10;
+var startSeconds = 10;
+
+var hours = startHours;
+var minutes = startMinutes;
+var seconds = startSeconds;
+
+
+var hoursDisplay = document.getElementById('hours');
+var minutesDisplay = document.getElementById('minutes');
+var secondsDisplay = document.getElementById('seconds');
+
+
+var timer = setInterval(function() {
+
+  var currentTime = Date.now();
+
+  var elapsedTime = currentTime - startTime;
+
+  var reverseTime = (startHours * 3600 + startMinutes * 60 + startSeconds) * 1000 - elapsedTime;
+
+  hours = Math.floor(reverseTime / (1000 * 60 * 60));
+  minutes = Math.floor((reverseTime % (1000 * 60 * 60)) / (1000 * 60));
+  seconds = Math.floor((reverseTime % (1000 * 60)) / 1000);
+
+  hoursDisplay.textContent = pad(hours);
+  minutesDisplay.textContent = pad(minutes);
+  secondsDisplay.textContent = pad(seconds);
+
+  if (reverseTime <= 0) {
+    clearInterval(timer);
+  }
+}, 1000);
+
+function pad(number) {
+  return (number < 10 ? '0' : '') + number;
+}
+
